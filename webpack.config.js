d7fc52e90c073018ead562b5c7f0e73ca9337e04
liuchengcheng1999 <liuchengcheng233@gmail.com>
2022-09-06 11:24:27 +0800

    修改了webpack配置

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, 'dist/lib'),
-        library: 'FUI',
+        library: 'random-react',
         libraryTarget: 'umd',
     },
     module: {
