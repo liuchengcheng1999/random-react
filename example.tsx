6ca572b9112099e0112bac8911c1e247f79a862a
liuchengcheng1999 <liuchengcheng233@gmail.com>
2022-09-13 22:26:33 +0800

    完成dialog组件的基本配置

--- example.tsx
+++ example.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import IconExample from './lib/icon/icon.example';
-import ButtonExample from './lib/button.example';
-
+import ButtonExample from './lib/button/button.example';
+import DialogExample from './lib/dialog/dialog.example';
 ReactDOM.render(
   <Router>
     <div>
@@ -22,12 +22,16 @@ ReactDOM.render(
             <li>
               <Link to="/button">Button</Link>
             </li>
+            <li>
+              <Link to="/dialog">Dialog</Link>
+            </li>
           </ul>
         </aside>
         <main>
           <Routes>
             <Route path="/icon" element={<IconExample />}/>
             <Route path="/button" element={<ButtonExample />}/>
+            <Route path="/dialog" element={<DialogExample />}/>
           </Routes>
         </main> 
       </div>
---
+++ lib/dialog/dialog.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+interface Props {
+    children?: React.ReactNode;
+    visible: boolean;
+}
+
+const Dialog: React.FunctionComponent<Props> = (props) => {
+    return (
+        props.visible ? 
+            <div>{props.children}</div>
+            : null
+            
+    )
+}
+export default Dialog;
