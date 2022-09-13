bdb3c17facaaa8d89e56a020a72ae40aa095ed44
liuchengcheng1999 <liuchengcheng233@gmail.com>
2022-09-13 16:50:47 +0800

    配置完成

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,9 +2,13 @@ const base = require('./webpack.config')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = Object.assign({}, base, {
     mode: 'development',
+    entry: {
+        example: './example.tsx',
+    },
+    stats: { children: false },
     plugins: [
         new HtmlWebpackPlugin({
-            template: 'index.html'
+            template: 'example.html'
         })
     ],
 })
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,30 @@
 const path = require('path')
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-    mode: 'production',
-    entry: {
-        index: './lib/index.tsx'
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.jsx'],
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist/lib'),
-        library: 'random-react',
-        libraryTarget: 'umd',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                loader: 'awesome-typescript-loader'
-            },
-            {
-                test: /\.svg$/,
-                loader: 'svg-sprite-loader',
-            },
-            {
-                test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
-            }
-        ]
-    }
+  entry: {
+    index: './lib/index.tsx'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist/lib'),
+    library: 'random-react',
+    libraryTarget: 'umd',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+      },
+      {
+        test: /\.svg$/,
+        loader: 'svg-sprite-loader',
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },
 }
